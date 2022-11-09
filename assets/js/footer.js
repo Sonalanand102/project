e94985943d4c19079e0a748f1e6b7629d86244ec
9GAG <sonalanand102@gmail.com>
2022-11-09 20:26:31 +0530

    contact page and company page updated

---
+++ assets/js/footer.js
@@ -0,0 +1,54 @@
+document.write(`
+<!-- Footer Section starts -->
+<div class="footer">
+    <div class="footer-section1">
+        <img src="assets/images/auxilogo.png" alt="">
+        <div class="footer_content">
+            We love to fabricate an ambience which looks dazzling and speaks melodiously. We are industrial pioneers backed by our existing clients.
+        </div>
+        <div class="social_icons">
+            <ul>
+                <li><img src="assets/images/Mask Group 297.png" alt=""></li>
+                <li><img src="assets/images/Mask Group 298.png" alt=""></li>
+                <li><img src="assets/images/Mask Group 299.png" alt=""></li>
+                <li><img src="assets/images/Mask Group 300.png" alt=""></li>
+            </ul>
+        </div>
+
+        <div class="copyrights">
+            Copyright ©2020 All rights reserved | This template is made with love by Marian
+        </div>
+    </div>
+    <div class="footer-section2">
+        <div class="information">
+            <h3>Information</h3>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="company.html">Company</a></li>
+                <li><a href="solution.html">Solution</a></li>
+                <li><a href="brand.html">Brand</a></li>
+                <li><a href="sector.html">Sector</a></li>
+                <li><a href="resources.html">Resources</a></li>
+                
+            </ul>
+        </div>
+        <div class="contact-info">
+            <h3>Contact Info</h3>
+            <div class="email">
+                <i class="fa-solid fa-house"></i>
+                info@auxibleindia.com
+            </div>
+            <div class="phone">
+                <i class="fa-solid fa-house"></i>
+                +91 7355751042 
+            </div>
+            <div class="address">
+                <i class="fa fa-house"></i>
+                Basement, H- 224 i, Sector 63,
+                Noida Uttar Pradesh 201301
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer Section ends -->
+`);
---
+++ assets/js/navbar.js
@@ -0,0 +1,22 @@
+document.write(`<!-- Navbar Starts-->
+<div class="navbar">
+
+    <!-- Company Logo -->
+    <div class="logo">
+       <img src="assets/images/Mask Group 291.png" alt="" >     
+    </div>
+
+    <!-- Menu -->
+    <div class="menu">
+        <ul>
+            <li class="home-section"><a class="active" href="index.html">Home</a></li>
+            <li class="company"><a href="company.html">Company</a></li>
+            <li class="solution"><a href="solution.html">Solution</a></li>
+            <li class="brand"><a href="brand.html">Brand</a></li>
+            <li class="sector"><a href="sector.html">Sector</a></li>
+            <li class="resources"><a href="resources.html">Resources</a></li>
+            <li class="contactus"><a href="contactUs.html">Contact Us</a></li>
+        </ul>
+    </div>
+</div>
+<!-- Navbar Ends-->`)
